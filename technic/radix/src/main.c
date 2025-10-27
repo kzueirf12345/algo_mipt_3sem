9c07bb17b3c91e2bc09192bde603125742a12c5f
kzueirf12345 <jaba0192837465@gmail.com>
2025-10-27 23:36:16 +0300

    vse taku stupid radix

--- technic/radix/src/main.c
+++ technic/radix/src/main.c
@@ -28,12 +28,26 @@ void fast_sort(unsigned *begin, unsigned *end)
 
     alignas(64) size_t count[256];
 
+    const size_t blockSize = 4096;
+
     for (int shift = 0; shift < 32; shift += 8) {
         memset(count, 0, sizeof(count));
 
-        for (size_t i = 0; i < n; ++i) {
-            unsigned byte = (src[i] >> shift) & 0xFF;
-            ++count[byte];
+        for (size_t start = 0; start < n; start += blockSize) {
+            size_t endBlock = (start + blockSize < n) ? start + blockSize : n;
+            size_t i = start;
+
+            for (; i + 3 < endBlock; i += 4) {
+                unsigned b0 = (src[i] >> shift) & 0xFF;
+                unsigned b1 = (src[i+1] >> shift) & 0xFF;
+                unsigned b2 = (src[i+2] >> shift) & 0xFF;
+                unsigned b3 = (src[i+3] >> shift) & 0xFF;
+
+                ++count[b0]; ++count[b1]; ++count[b2]; ++count[b3];
+            }
+            for (; i < endBlock; ++i) {
+                ++count[(src[i] >> shift) & 0xFF];
+            }
         }
 
         size_t sum = 0;
@@ -43,9 +57,25 @@ void fast_sort(unsigned *begin, unsigned *end)
             sum += tmp;
         }
 
-        for (size_t i = 0; i < n; ++i) {
-            unsigned byte = (src[i] >> shift) & 0xFF;
-            dst[count[byte]++] = src[i];
+        for (size_t start = 0; start < n; start += blockSize) {
+            size_t endBlock = (start + blockSize < n) ? start + blockSize : n;
+            size_t i = start;
+
+            for (; i + 3 < endBlock; i += 4) {
+                unsigned b0 = (src[i] >> shift) & 0xFF;
+                unsigned b1 = (src[i+1] >> shift) & 0xFF;
+                unsigned b2 = (src[i+2] >> shift) & 0xFF;
+                unsigned b3 = (src[i+3] >> shift) & 0xFF;
+
+                dst[count[b0]++] = src[i];
+                dst[count[b1]++] = src[i+1];
+                dst[count[b2]++] = src[i+2];
+                dst[count[b3]++] = src[i+3];
+            }
+            for (; i < endBlock; ++i) {
+                unsigned b = (src[i] >> shift) & 0xFF;
+                dst[count[b]++] = src[i];
+            }
         }
 
         unsigned *tmpPtr = src;

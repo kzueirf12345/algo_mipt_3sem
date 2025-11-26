7f7ccd2c377d99da9eecdae75b5b433626d62fab
kzueirf12345 <jaba0192837465@gmail.com>
2025-11-27 02:46:16 +0300

    I

---
+++ 3/I/src/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+// #include <unordered_set>
+#include <set>
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    size_t N = 0, M = 0;
+    std::cin >> N >> M;
+
+    std::vector<std::set<long long>> sets(N + 1);
+    std::unordered_map<long long, std::set<size_t>> belongs;
+
+    for (size_t i = 0; i < M; ++i) {
+        std::string cmd;
+        std::cin >> cmd;
+
+        if (cmd == "+") {
+            long long x = 0; 
+            size_t s = 0;
+            std::cin >> x >> s;
+
+            if (sets[s].insert(x).second) {
+                belongs[x].insert(s);
+            }
+        } else if (cmd == "-") {
+            long long x = 0; 
+            size_t s = 0;
+            std::cin >> x >> s;
+
+            if (sets[s].erase(x)) {
+                belongs[x].erase(s);
+                if (belongs[x].empty()) {
+                    belongs.erase(x);
+                }
+            }
+        } else if (cmd == "=") {
+            size_t s = 0;
+            std::cin >> s;
+
+            for (long long x : sets[s]) {
+                belongs[x].erase(s);
+                if (belongs[x].empty()) {
+                    belongs.erase(x);
+                }
+            }
+            sets[s].clear();
+
+        } else if (cmd == "?") {
+            size_t s = 0;
+            std::cin >> s;
+
+            if (sets[s].empty()) {
+                std::cout << -1 << "\n";
+            } else {
+                for (long long x : sets[s]) {
+                    std::cout << x << " ";
+                }
+                std::cout << "\n";
+            }
+        } else if (cmd == "??") {
+            long long x = 0;
+            std::cin >> x;
+
+            if (belongs.count(x) == 0 || belongs[x].empty()) {
+                std::cout << -1 << "\n";
+            } else {
+                for (size_t s : belongs[x]) {
+                    std::cout << s << " ";
+                }
+                std::cout << "\n";
+            }
+        }
+    }
+}
